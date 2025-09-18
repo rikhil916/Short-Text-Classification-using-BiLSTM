dd2d51421d9bcd4e5e55ffca9a7ceda95f7a4bc9
Chetan Rikhil <chetanrikhil916@gmail.com>
2025-09-18 11:29:56 +0530

    bug fixed

--- predict.py
+++ predict.py
@@ -1,28 +1,35 @@
-import re
-import pickle
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+import json
+import re
+import tensorflow as tf
+import keras
+print(tf.__version__)
+print(keras.__version__)
 
-try:
-    model = load_model("spam_ham_classifier_bilstm.h5", compile=False)
-except Exception as e:
-    print(f"Error loading model: {e}")
+from keras.models import load_model
+import pickle
 
-with open("token.pkl","rb") as f:
-    tokenizer=pickle.load(f)
-    
+model = load_model("spam_ham_classifier_bilstm.keras")
+
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+
+# Clean text function
 def clean_text(text):
     text = text.lower()
     text = re.sub(r"http\S+", "", text)
     text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
-    return text 
+    return text
 
+# Prediction function
 def predict_spam(text):
     cleaned = clean_text(text)
     seq = tokenizer.texts_to_sequences([cleaned])
-    padded = pad_sequences(seq, maxlen=100, padding='post', truncating='post')
-    pred = model.predict(padded)[0][0]
+    padded_seq = pad_sequences(seq, maxlen=100, padding='post', truncating='post')
+    pred = model.predict(padded_seq)[0][0]
     return "Spam" if pred > 0.5 else "Ham"
 
-print(predict_spam("Congratulations! You've won a ₹5000 gift card."))
-print(predict_spam("Hey, let's catch up later today."))
+# Example
+print(predict_spam("Congratulations! You've won a free iPhone!"))
